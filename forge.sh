55be97e99bc4a2f9fd8381c1d2265b89d19996f8
legendofmiracles <legendofmiracles@protonmail.com>
2021-01-16 18:50:51 +0000

    now works with all versions and also displays 1.16.4

--- forge.sh
+++ forge.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
-selection=$(./versions.sh | fzf)
-url=$(curl "https://files.minecraftforge.net/maven/net/minecraftforge/forge/index_$selection.html" 2> /dev/null | xmllint --html --xpath "string(/html/body/main/div[2]/div[1]/div[2]/div/div[1]/div[2]/div[3]/a/@href)" - 2> /dev/null | cut -d "=" -f 3)
+set -x
+selection=$(./versions.sh | sort -uVr |  fzf)
+url=$(curl "https://files.minecraftforge.net/maven/net/minecraftforge/forge/index_$selection.html" 2> /dev/null | xmllint --html --xpath 'string(/html/body/main/div[2]/div[1]/div[2]/div/div[1]/div[2]//*[@title = "Installer"]/@href)' - 2> /dev/null | cut -d "=" -f 3)
+echo $url
 curl $url -o forge.jar
 java -jar forge.jar
