f6a7238bc3037bd1168d3aabb6233f1419f3e629
legendofmiracles <legendofmiracles@protonmail.com>
2021-01-16 19:12:57 +0000

    one file now

--- forge.sh
+++ forge.sh
@@ -1,7 +1,17 @@
 #!/bin/env bash
-set -x
-selection=$(./versions.sh | sort -uVr |  fzf)
+function versions() {
+  list=$(curl https://files.minecraftforge.net/ -q 2> /dev/null | xmllint --xpath "/html/body/main/div[1]/aside/section//li" --html - 2> /dev/null | /bin/grep "<a href=\"/\|elem-active")
+  versions=""
+  while IFS= read -r line; do
+    versions="${versions}$(printf "$line" | xmllint --xpath "string(.)" -)\n"
+  done <<< "$list"
+}
+
+versions
+selection=$(printf "$versions" | sort -uVr |  fzf)
 url=$(curl "https://files.minecraftforge.net/maven/net/minecraftforge/forge/index_$selection.html" 2> /dev/null | xmllint --html --xpath 'string(/html/body/main/div[2]/div[1]/div[2]/div/div[1]/div[2]//*[@title = "Installer"]/@href)' - 2> /dev/null | cut -d "=" -f 3)
 echo $url
 curl $url -o forge.jar
 java -jar forge.jar
+
+
--- versions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-#set -x
-list=$(curl https://files.minecraftforge.net/ -q 2> /dev/null | xmllint --xpath "/html/body/main/div[1]/aside/section//li" --html - 2> /dev/null | /bin/grep "<a href=\"/\|elem-active")
-while IFS= read -r line; do
-  printf "$line" | xmllint --xpath "string(.)" -
-done <<< "$list"
